05f3a495c31bec78155f1a6ad8d02ac8e8b81e4d
William Gyasi <kwabenagyasi63@gmail.com>
2021-10-21 20:11:12 +0000

    Adding Styles Directory

---
+++ src/newpage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Homepage=()=>{
+    return(
+        
+    <div className="homepag">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="titile">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="titile">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="titile">WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="titile">SNEAKERS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="titile">MENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+)
+}
+
+export default Homepage;
