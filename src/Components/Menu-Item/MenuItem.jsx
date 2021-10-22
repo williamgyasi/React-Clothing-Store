3ebe96248148d34a8825b547a00a8caa26b60d9d
William Gyasi <kwabenagyasi63@gmail.com>
2021-10-22 00:51:05 +0000

    Adding Router

--- src/Components/Menu-Item/MenuItem.jsx
+++ src/Components/Menu-Item/MenuItem.jsx
@@ -3,10 +3,12 @@ import './menuItem.style.scss'
 
 const MenuItem = ({title,imageUrl,size}) => {
   return (
-    <div style={{
-      backgroundImage:`url(${imageUrl})`,
-      height:size?'320px':null
-    }} className={`menu-item`}>
+    <div  className={`${size} menu-item`}>
+      <div 
+      style={{
+        backgroundImage:`url(${imageUrl})`,
+      }}
+      className="background-image"></div>
       <div className="content">
         <h1 className="content-title">{title}</h1>
         <span className="content-subtitle">SHOP NOW</span>
